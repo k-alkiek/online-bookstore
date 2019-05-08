999391ea91291c91b1998c6242f008afc822612e
Merit-Victor <meritsvictor1.618@gmail.com>
2019-05-08 19:22:06 +0200

    Add quantity field.

--- online-bookstore/app/controllers/cart_controller.rb
+++ online-bookstore/app/controllers/cart_controller.rb
@@ -6,24 +6,22 @@ class CartController < ApplicationController
  	p = ActiveRecord::Base.establish_connection
  	c = p.connection
   	@rows = []
-	@cart_books.each do |book|
-		result = c.execute("select ISBN,title,category,selling_price,PUBLISHER_Name,publish_year from BOOK where ISBN =  \"#{book}\"")
-		@rows.push result.first
-	end
+  	@cart_books = @cart_books.map{|e| "\"#{e}\"" }
+  	str = @cart_books.join(",")
+	@result = c.execute("select ISBN,title,category,selling_price,Available_copies_count,PUBLISHER_Name,publish_year from BOOK where ISBN IN (#{str})")
   end
 
   def add_to_cart
-    @cart_books.push params[:ISBN]
-    @cart_books_arr = @cart_books.join(",")
-    @quantity += 1
-    cookies.permanent.signed[:no_of_books] = @quantity
-    cookies.permanent[:books_in_cart] = @cart_books_arr
+  	if !@cart_books.include? params[:ISBN]
+  		@cart_books.push params[:ISBN]
+  	end    
+    cart_books_str = @cart_books.join(",")
+    cookies.permanent[:books_in_cart] = cart_books_str
     redirect_to action: 'show'
   end
 
   private
   def set_cart_books
   	@cart_books = cookies[:books_in_cart] ? cookies[:books_in_cart].split(",") : []
-  	@quantity = cookies[:no_of_books].to_i ? cookies[:no_of_books] : 0
   end
 end
